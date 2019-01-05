8c885fef84dc28da3333a32b8cc7c3fbefb306da
Tatiana <tnino@homeaway.com>
2019-01-05 00:35:03 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chorus = "Let's dance!";
+var repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("*change key*");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
